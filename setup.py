ae81db0fc8e85a08239457f8e764e7c14b2d5ac2
aashi0114 <aashigupta0114@gmail.com>
2024-07-11 12:25:14 -0700

    created summary file

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import os
 #creating the treefam-pdb
-os.mkdir("treefam-pdb"
+os.mkdir("treefam-pdb")
+
+os.mkdir("treefam-pdb/treefam alignments and trees")
+os.mkdir("treefam-pdb/pdb structure files")
+os.mkdir("treefam-pdb/contact matrices")
+os.mkdir("treefam-pdb/treefam-pdb mappings")
